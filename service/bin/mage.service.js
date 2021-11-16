c3288efdba181214caf38bdddb94d020a6a61dc1
Robert St. John <restjohn@users.noreply.github.com>
2021-11-16 13:03:02 -0700

    [service] remove comma in bin script for consistency

--- service/bin/mage.service.js
+++ service/bin/mage.service.js
@@ -31,7 +31,7 @@ const optPluginsDesc =
 `;
 
 const options = [
-  new Option('-C, --config <string>', optConfigDesc).env('MAGE_CONFIG').argParser(parseConfigJsonFromStringOrFile),
+  new Option('-C --config <string>', optConfigDesc).env('MAGE_CONFIG').argParser(parseConfigJsonFromStringOrFile),
   new Option('-a --address <string>', 'The address on which the web server listens for HTTP requests').env('MAGE_ADDRESS').default('0.0.0.0'),
   new Option('-p --port <number>', 'The port on which which the web server listens for HTTP requests').env('MAGE_PORT').default(4242).argParser(x => parseInt(x)),
   new Option('--attachment-dir <string>').env('MAGE_ATTACHMENT_DIR').default('/var/lib/mage/attachments'),
