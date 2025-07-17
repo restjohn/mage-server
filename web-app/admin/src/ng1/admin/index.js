abb6ec331a714c2081ea25a6754a6e9d52df3210
MilliannaMiodori <cozycollectorclub@gmail.com>
2025-07-17 12:05:46 -0600

    directive

--- web-app/admin/src/ng1/admin/index.js
+++ web-app/admin/src/ng1/admin/index.js
@@ -1,8 +1,8 @@
 import angular from 'angular';
 import admin from './admin.component';
 import adminTab from './admin.tab.component';
-import adminDashboard from './admin.dashboard.component';
 import adminPluginTabContentBridge from './admin.plugin-tab-content-bridge.component'
+import { AdminDashboardComponent } from './admin-dashboard';
 import { downgradeComponent } from '@angular/upgrade/static';
 import { AdminPluginTabContentComponent } from '../../app/admin/plugin-tab/plugin-tab-content.component';
 
@@ -10,7 +10,7 @@ angular
   .module('mage')
   .component('admin', admin)
   .component('adminTab', adminTab)
-  .component('adminDashboard', adminDashboard)
+  .directive('adminDashboard', downgradeComponent({ component: AdminPluginTabContentComponent }))
   .component('mageAdminPluginTabContentBridge', adminPluginTabContentBridge)
   .directive('mageAdminPluginTabContent', downgradeComponent({ component: AdminPluginTabContentComponent }));
 
